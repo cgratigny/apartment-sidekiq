358f7f043ac172df24eae5852790b76d253cdb2b
Chris Gratigny <chrisg@ibethel.org>
2019-10-27 17:27:24 -0700

    Remove server.

--- lib/apartment/sidekiq/middleware/server.rb
+++
@@ -1,9 +0,0 @@
-module Apartment::Sidekiq::Middleware
-  class Server
-    def call(worker_class, item, queue)
-      Apartment::Tenant.switch(item['apartment']) do
-        yield
-      end
-    end
-  end
-end
