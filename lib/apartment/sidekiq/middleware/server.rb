4a75d214e309c668dd54abac576e5dc298dba359
Chris Gratigny <chrisg@ibethel.org>
2019-10-27 17:29:33 -0700

    Switch how switch happens.

--- lib/apartment/sidekiq/middleware/server.rb
+++ lib/apartment/sidekiq/middleware/server.rb
@@ -1,9 +1,8 @@
 module Apartment::Sidekiq::Middleware
   class Server
     def call(worker_class, item, queue)
-      Apartment::Tenant.switch(item['apartment']) do
-        yield
-      end
+      Apartment::Tenant.switch!(item['apartment'])
+      yield
     end
   end
 end
